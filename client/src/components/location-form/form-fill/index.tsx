086e721d0baa2dbd07c63dce5cd043cad061be0b
e.qorri <e.qorri@teamsystem.com>
2024-04-03 22:41:38 +0200

    fixed form location

--- client/src/components/location-form/form-fill/index.tsx
+++ client/src/components/location-form/form-fill/index.tsx
@@ -1,21 +1,54 @@
 import {
   Button,
+  FormControl,
   InputLabel,
+  MenuItem,
+  Select,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
+import { useEffect, useState } from "react";
+import dayjs, { Dayjs } from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-const FormFIll = () => {
+interface Country {
+  name: {
+    common: string;
+  };
+}
+
+const inputLabelStyle = { color: "black", padding: "7px" };
+
+const FormFill = () => {
+  const [cities, setCities] = useState([]);
+  const [value, setValue] = useState<Dayjs | null>(dayjs("2022-04-17"));
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        const countryNames = data.map(
+          (country: Country) => country.name.common
+        );
+        setCities(countryNames);
+      });
+  }, []);
+
   return (
     <Stack
       sx={{
         background: "#1089ff",
         height: "550px",
-        width: "370px",
         padding: "30px",
         borderRadius: "10px",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "400px",
       }}
+      spacing={2}
     >
       <Typography
         sx={{
@@ -29,86 +62,92 @@ const FormFIll = () => {
       >
         Make your Trip
       </Typography>
-      <Stack sx={{ gap: 2 }}>
+      <FormControl fullWidth>
+        <InputLabel sx={{ ...inputLabelStyle }} id="demo-simple-select-label">
+          Pick-up Location
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Pick-up Location"
+        >
+          {cities.map((city, index) => (
+            <MenuItem key={index} value={city}>
+              {city}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel color="primary" id="demo-simple-select-label">
+          Drop-off Location
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Drop-off Location"
+        >
+          {cities.map((city, index) => (
+            <MenuItem key={index} value={city}>
+              {city}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Stack direction="row" spacing={2}>
         <Stack>
           <InputLabel
-            sx={{ color: "white", padding: "7px" }}
-            htmlFor="pickup-location"
+            sx={{ inputLabelStyle, padding: "20px" }}
+            htmlFor="pick-up-date"
           >
-            PICK-UP LOCATION
+            PICK-UP DATE
           </InputLabel>
-          <TextField
-            InputProps={{
-              style: { color: "white" },
-            }}
-            id="pickup-location"
-            placeholder="City, Airport, ecc"
-          />
-        </Stack>{" "}
-        <Stack>
-          <InputLabel
-            sx={{ color: "white", padding: "7px" }}
-            htmlFor="DROP-OFF LOCATION"
-          >
-            DROP-OFF LOCATION
-          </InputLabel>
-          <TextField
-            InputProps={{
-              style: { color: "white" },
-            }}
-            id="DROP-OFF LOCATION"
-            placeholder="City, Airport, ecc"
-          />
-        </Stack>{" "}
-        <Stack direction="row" justifyContent="space-between">
-          <Stack marginRight="20px">
-            <InputLabel
-              sx={{ fontSize: "12px", color: "white", padding: "7px" }}
-              htmlFor="PICK-UP DATE"
-            >
-              PICK-UP DATE
-            </InputLabel>
-            <TextField
-              InputProps={{
-                style: { color: "white" },
-              }}
-              id="PICK-UP DATE"
-              placeholder="Date"
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="PICK-UP DATE"
+              defaultValue={dayjs("2022-04-17")}
+              onChange={(newValue) => setValue(newValue)}
             />
-          </Stack>
-          <Stack>
-            <InputLabel
-              sx={{ fontSize: "12px", color: "white", padding: "7px" }}
-              htmlFor="DROP-OFF DATE"
-            >
-              DROP-OFF DATE
-            </InputLabel>
-            <TextField
-              InputProps={{
-                style: { color: "white" },
-              }}
-              id="DROP-OFF DATE"
-              placeholder="Date"
-            />
-          </Stack>
+          </LocalizationProvider>
         </Stack>
+
         <Stack>
           <InputLabel
-            sx={{ color: "white", padding: "7px" }}
-            htmlFor="pickup-location"
+            sx={{ inputLabelStyle, padding: "20px" }}
+            htmlFor="drop-off-date"
           >
-            PICK-UP LOCATION
+            DROP-OFF DATE
+          </InputLabel>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="DROP-OFF DATE"
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+            />
+          </LocalizationProvider>
+          <InputLabel
+            sx={{ ...inputLabelStyle, marginBottom: "10px" }}
+            htmlFor="pick-up-time"
+          >
+            PICK-UP TIME
           </InputLabel>
           <TextField
-            InputProps={{
-              style: { color: "white" },
+            fullWidth
+            id="pick-up-time"
+            type="time"
+            defaultValue="07:30"
+            InputLabelProps={{
+              shrink: true,
             }}
-            id="pickup-location"
-            placeholder="City, Airport, ecc"
+            inputProps={{
+              step: 300, // 5 min
+            }}
+            sx={{ alignSelf: "flex-start" }}
           />
         </Stack>
       </Stack>
       <Stack
+        width="50%"
         marginTop="20px"
         sx={{ background: "#01d28e", borderRadius: "10px" }}
       >
@@ -120,4 +159,4 @@ const FormFIll = () => {
   );
 };
 
-export default FormFIll;
+export default FormFill;
